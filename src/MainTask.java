4afe5ec12bea93e29f6da77d5b3544e2962fb50b
kanmpatel <kanm2618@gmail.com>
2022-05-30 22:55:57 +0100

    this is done by megha

--- src/MainTask.java
+++ src/MainTask.java
@@ -2,6 +2,5 @@ public class MainTask {
     public static void main(String[] args) {
         System.out.println(" this is done by kinjal");
         System.out.println("this is again done by megha");
-
     }
 }
