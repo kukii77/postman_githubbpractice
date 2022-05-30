855d9934c4746ad708572c276b808ff5b4fcc1dc
kukii77 <desty9008@gmail.com>
2022-05-30 21:33:57 +0100

    this is my first commit

---
+++ src/MainTask.java
@@ -0,0 +1,5 @@
+public class MainTask {
+    public static void main(String[] args) {
+        System.out.println(" this is done by kinjal");
+    }
+}
---
+++ src/SecondClass.java
@@ -0,0 +1,5 @@
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("this is my second class By kinjal");
+    }
+}
